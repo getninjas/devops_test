54af23201bf7424fe0de74c0f2bce1f37ade2fbe
Adelson Junior <adelson.oliveira@getninjas.com.br>
2018-09-05 14:03:54 -0300

    Test

---
+++ main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "os"
+    "net/http"
+    "net/http/httptest"
+    "testing"
+)
+
+func TestHealthCheck(t *testing.T) {
+    // Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+    // pass 'nil' as the third parameter.
+    req, err := http.NewRequest("GET", "/healthcheck", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    // We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+    os.Setenv("APP_NAME", "mock")
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(HealthCheck)
+
+    // Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+    // directly and pass in our Request and ResponseRecorder.
+    handler.ServeHTTP(rr, req)
+
+    // Check the status code is what we expect.
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("handler returned wrong status code: got %v want %v",
+            status, http.StatusOK)
+    }
+
+    // Check the response body is what we expect.
+    expected := `Hey Bro, mock is Alive!`
+    if rr.Body.String() != expected {
+        t.Errorf("handler returned unexpected body: got %v want %v",
+            rr.Body.String(), expected)
+    }
+}
